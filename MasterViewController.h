7cdc4b0bfed842ca2348879ec0c8668df3b6f933
Anthony Dagati <tdagati.cs@gmail.com>
2014-11-13 07:43:37 -0800

    First commit of FailedBankCD2

---
+++ MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  FailedBankCD2
+//
+//  Created by Anthony Dagati on 11/13/14.
+//  Copyright (c) 2014 Black Rail Capital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@property (nonatomic, strong) NSManagedObjectContext *managedObjectContext;
+
+@end
